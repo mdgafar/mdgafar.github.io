38971008b94a1a002cabb5dc285367b84f66007f
mdgafar <113158316+mdgafar@users.noreply.github.com>
2022-09-20 17:13:09 +0545

    Create form1.php

---
+++ form1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
