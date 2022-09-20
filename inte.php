abec3539c65c8990f7f83c8cae4fec66cb3cd819
mdgafar <113158316+mdgafar@users.noreply.github.com>
2022-09-20 14:32:04 +0545

    Create inte.php

---
+++ inte.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Check if the type of a variable is integer   
+$x = 5985;
+var_dump(is_int($x));
+
+echo "<br>";
+
+// Check again... 
+$x = 59.85;
+var_dump(is_int($x));
+?>  
+
+</body>
+</html>
